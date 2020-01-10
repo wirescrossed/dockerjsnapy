92bc131592c04177cecacc86d436b1114b9749d8
Lance Le Roux <wirescrossed@gmail.com>
2020-01-10 10:20:58 -0500

    Ignore Python Warnings

--- jsnapy.sh
+++ jsnapy.sh
@@ -1,1 +1,1 @@
-docker run -i -t -v $PWD/snapshots:/root/jsnapy/snapshots/ -v $PWD/tests:/usr/src/app wirescrossed/jsnapy:latest "$@"
+docker run -i -t -e PYTHONWARNINGS="ignore" -v $PWD/snapshots:/root/jsnapy/snapshots/ -v $PWD/tests:/usr/src/app wirescrossed/jsnapy:latest "$@"
